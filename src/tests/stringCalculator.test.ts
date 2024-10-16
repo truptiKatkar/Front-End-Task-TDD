a45bf60c779efd5ad266c51995fdc6edd89c5d82
truptiKatkar <trupti1katkar@gmail.com>
2024-10-17 00:53:16 +0530

    added remaining test cases

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -16,3 +16,14 @@ test('should handle new lines between numbers', () => {
   expect(add("1\n2,3")).toBe(6);
 });
 
+test('should support different delimiters', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
+
+test('should throw an error for negative numbers', () => {
+  expect(() => add("1,-2,3")).toThrow("negative numbers not allowed -2");
+});
+
+test('should list all negative numbers in the exception', () => {
+  expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed -2, -3");
+});
