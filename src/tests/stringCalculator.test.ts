c9c0fe2ca4453c7b8ee8f045ae4594a4e58713d4
truptiKatkar <trupti1katkar@gmail.com>
2024-10-16 23:37:48 +0530

    added test cases

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -4,4 +4,15 @@ test('should return 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
 
+test('should return the number itself when input is a single number', () => {
+  expect(add("1")).toBe(1);
+});
+
+test('should return the sum of two comma-separated numbers', () => {
+  expect(add("1,5")).toBe(6);
+});
+
+test('should handle new lines between numbers', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
 
